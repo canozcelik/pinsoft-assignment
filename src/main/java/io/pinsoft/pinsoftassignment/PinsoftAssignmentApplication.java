c72f7faea20787173517d11e4ee116be5a8964a7
Can <canozcelik@live.com>
2023-08-29 14:23:51 +0300

    initial commit

---
+++ src/main/java/io/pinsoft/pinsoftassignment/PinsoftAssignmentApplication.java
@@ -0,0 +1,13 @@
+package io.pinsoft.pinsoftassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinsoftAssignmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PinsoftAssignmentApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/pinsoft/pinsoftassignment/PinsoftAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pinsoft.pinsoftassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinsoftAssignmentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
