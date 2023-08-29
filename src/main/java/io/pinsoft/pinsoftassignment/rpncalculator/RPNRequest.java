0b33f231da15f5bfb92264fc8fdae8a2b8f509ad
Can <canozcelik@live.com>
2023-08-29 23:50:23 +0300

    rpn request and response were added.

---
+++ src/main/java/io/pinsoft/pinsoftassignment/rpncalculator/RPNRequest.java
@@ -0,0 +1,14 @@
+package io.pinsoft.pinsoftassignment.rpncalculator;
+
+public class RPNRequest {
+
+  private String expression;
+
+  public String getExpression() {
+    return expression;
+  }
+
+  public void setExpression(String expression) {
+    this.expression = expression;
+  }
+}
---
+++ src/main/java/io/pinsoft/pinsoftassignment/rpncalculator/RPNResponse.java
@@ -0,0 +1,14 @@
+package io.pinsoft.pinsoftassignment.rpncalculator;
+
+public class RPNResponse {
+
+  private Integer result;
+
+  public Integer getResult() {
+    return result;
+  }
+
+  public void setResult(Integer result) {
+    this.result = result;
+  }
+}
