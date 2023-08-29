26ad28565d70399b706bc40aeb9a0f41c4f22e7a
Can <canozcelik@live.com>
2023-08-30 00:35:39 +0300

    rpn service and its implementation were added.

---
+++ src/main/java/io/pinsoft/pinsoftassignment/rpncalculator/RPNService.java
@@ -0,0 +1,7 @@
+package io.pinsoft.pinsoftassignment.rpncalculator;
+
+public interface RPNService {
+
+  Integer calculate(String expression);
+
+}
---
+++ src/main/java/io/pinsoft/pinsoftassignment/rpncalculator/RPNServiceImpl.java
@@ -0,0 +1,12 @@
+package io.pinsoft.pinsoftassignment.rpncalculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RPNServiceImpl implements RPNService{
+
+  @Override
+  public Integer calculate(String expression) {
+    return null;
+  }
+}
