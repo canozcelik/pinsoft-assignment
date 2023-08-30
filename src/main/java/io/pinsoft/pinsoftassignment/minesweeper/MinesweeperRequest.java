3fdd87d26e55d374f30c5f42738de428f6f8d48c
Can <canozcelik@live.com>
2023-08-30 14:09:56 +0300

    minesweeper request and response were added.

---
+++ src/main/java/io/pinsoft/pinsoftassignment/minesweeper/MinesweeperRequest.java
@@ -0,0 +1,13 @@
+package io.pinsoft.pinsoftassignment.minesweeper;
+
+public class MinesweeperRequest {
+  private String[] square;
+
+  public String[] getSquare() {
+    return square;
+  }
+
+  public void setSquare(String[] square) {
+    this.square = square;
+  }
+}
